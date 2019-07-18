109772988ed60d1984d2dd6d23ad70d0259182c1
Amruth Pillai <im.amruth@gmail.com>
2019-07-19 00:39:30 +0530

    validation using pipes and class-validator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Delete,
   Patch,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
@@ -32,6 +34,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
     return this.tasksService.createTask(createTaskDTO);
   }
